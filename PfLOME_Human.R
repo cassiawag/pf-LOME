d0c2ffcccd8994bae8fb5526ad9090239a24e0dc
cassiawag <cassia.wagner9@gmail.com>
2021-12-10 10:34:27 -0800

    rm everything but PfLOME

---
+++ PfLOME_Human.R
@@ -0,0 +1,209 @@
+source("PfLOME_HealthState.R")
+source("PfLOME_ImmuneState.R")
+
+Human <- R6Class("Human",
+                 
+                 ## Public Fields, Methods, and Initialization
+                 public = list(
+                   
+                   ## Initialization of Components
+                   
+                   initialize = function(ixH = NA, age = NA, sex = NA, locH = NA, IncImm=T, IncPfPed = T){
+                     private$ixH = ixH
+                     private$age = age
+                     private$sex = sex
+                     private$locH = locH
+                     private$pathogen = Pathogen$new()
+                     private$IncImm = IncImm
+                     private$IncPfPed = IncPfPed
+                     if(private$IncImm == T){
+                        private$immuneState = ImmuneState$new()
+                     }
+                     private$healthState = HealthState$new()
+              
+                   },
+                   
+                   
+                   ######## Infection Methods #########
+                   
+                   
+                   infectHuman = function(t,pfid){
+                     ##if no pedigree, only need to add a mostly empty pathogen; no recording
+                     if(private$IncPfPed == F){
+                       private$pathogen$add_Pf(t,pfid,NULL,NULL,NULL,0)
+                     }
+                     
+                     ##if pedigree, need to trace and record lineage
+                     if(private$IncPfPed == T){
+                        mic = pfped$get_mic(pfid)
+                        mac = pfped$get_mac(pfid)
+                        gtype = pfped$get_gtype(pfid)
+                        if(private$IncImm == T){ 
+                            BSImm = private$immuneState$get_BSImm()
+                        }
+                        if(private$IncImm == F){
+                          BSImm = 0
+                        }
+                        ##type immunity - currently turned off
+                        #typeImm = private$immuneState$get_typeImm(t,ptype)
+                        private$pathogen$add_Pf(t,pfid,mic,mac,gtype,BSImm)
+                        pfped$set_th(pfid,t)
+                     }
+                     
+                   },
+                   
+                   ## write method to remove particular infection
+                   ## **not currently used **
+                   clearPathogen = function(t, pfid){
+                     private$pathogen$PfPathogen[[pfid]] = NULL
+                     private$pathogen$set_PfMOI(private$pathogen$get_PfMOI()-1)
+                     pfped$set_thEnd(pfid,t)
+                   },
+                   infectMosquito = function(t, pfid, ixm){
+                     
+                   },
+                   
+                   moveHuman = function(newlocH){
+                     self$set_locH(newlocH)
+                   },
+                   
+                   Treat = function(t,Drug){
+                     private$healthState$Treat(t,Drug)
+                   },
+                   
+                   ########## Update Function #########
+                   
+                   
+                   updateHuman = function(t,dt){
+                     ##only update immunity if included
+                     if(private$IncImm == T){
+                        private$immuneState$update_immuneState(t,dt,self$get_Ptot(),self$get_Gtot())
+                     }
+                     private$healthState$update_healthState(t,dt,self$get_Ptot(),self$get_history()$RBC)
+                     private$pathogen$update_pathogen(t,dt,self$get_PD())
+                   },
+                   
+                   
+                   ########## Accessors ##############
+                   
+                   
+                   get_ixH = function(){
+                     private$ixH
+                   },
+                   
+                   set_ixH = function(newixH){
+                     private$ixH = newixH
+                   },
+                   
+                   get_age = function(){
+                     private$age
+                   },
+                   
+                   set_age = function(newAge){
+                     private$age = newAge
+                   },
+                   
+                   get_sex = function(){
+                     private$sex
+                   },
+                   
+                   set_sex = function(newSex){
+                     private$sex = newSex
+                   },
+                   
+                   get_locH = function(){
+                     private$locH
+                   },
+                   
+                   set_locH = function(newlocH){
+                     private$locH = newlocH
+                   },
+                   
+                   get_immuneState = function(){
+                     if(private$IncImm == T){
+                        return(private$immuneState)
+                     }
+                     if(private$IncImm == F){
+                       return(private$IncImm)
+                     }
+                   },
+                   
+                   get_healthState = function(){
+                     private$healthState
+                   },
+                   
+                   get_pathogen = function(){
+                     private$pathogen
+                   },
+                   
+                   get_HRP2 = function(){
+                     private$healthState$get_HRP2()
+                   },
+                   
+                   get_pLDH = function(){
+                     private$healthState$get_pLDH()
+                   },
+                   
+                   get_Ptot = function(){
+                     private$pathogen$get_Ptot()
+                   },
+                   
+                   get_Gtot = function(){
+                     private$pathogen$get_Gtot()
+                   },
+                   
+                   get_Drug = function(){
+                     private$healthState$get_Drug()
+                   },
+                   
+                   get_RxStart = function(){
+                     private$healthState$get_RxStart()
+                   },
+                   
+                   get_PD = function(){
+                     private$healthState$get_PD()
+                   },
+                   
+                   get_Fever = function(){
+                     private$healthState$get_Fever()
+                   },
+                   
+                   get_PfMOI = function(){
+                     private$pathogen$get_PfMOI()
+                   },
+                   
+                   get_history = function(){
+                     if(private$IncImm==T){
+                        c(private$pathogen$get_history(), 
+                          private$healthState$get_history(),
+                          private$immuneState$get_history())
+                     }
+                     if(private$IncImm==F){
+                        c(private$pathogen$get_history(), 
+                         private$healthState$get_history())
+                     }
+                   },
+                   
+                   get_IncImm = function(){
+                     private$IncImm
+                   }
+                   
+                   
+                 ),
+                 
+                 ## Private Fields
+                 private = list(
+                   
+                   ixH = NULL,
+                   age = NULL,
+                   sex = NULL,
+                   locH = NULL,
+                   IncPfPed = NULL,
+                   pathogen = NULL,
+                   IncImm = NULL,
+                   immuneState = NULL,
+                   healthState = NULL
+                   
+                 )
+                 
+)
---
+++ PfLOME_Mosquito.R
@@ -0,0 +1,51 @@
+Mosquito <- R6Class("Mosquito",
+                       
+                       public = list(
+                         
+                         initialize = function(){
+
+                         },
+                         
+                         
+                         ####### Accessors ########
+
+                         
+                         
+                         ############ Update Methods ##############
+                         
+                         update_SPZ = function(){
+                           
+                         },
+                         
+                         gr_tent = function(t, PAR){with(PAR,{
+                           ifelse(t<peakD, gr, 0)
+                         })},
+                         
+                         dPdt_tent = function(t, P, PAR, PD=0, IM=0){with(PAR,{
+                           age = ifelse(t>=t0, t-t0+1, 0)
+                           P = ifelse(age>=1 & age<=tEnd,
+                                      pmin(mxPD, P + self$gr_tent(age,PAR))-PD-IM,
+                                      NaN)
+                           ifelse(!is.na(P)&P>0, P, NaN)
+                         })},
+                         
+                         
+                         update_history = function(){
+
+                         }
+                         
+                         
+                         
+                       ),
+                    
+                    ################ Infection Methods ###################
+                       
+                       
+                       private = list(
+                         SPZ = NULL,
+                         pfidList = NULL,
+                         nOok = NULL,
+                         MOI = NULL
+                       )
+)
+                       
